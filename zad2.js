0158bf40b769133a662ad962a529ad04a77d6132
michaelloo35 <michaelloo35@gmail.com>
2017-11-23 04:52:48 +0100

    corrected sync algorithm

--- zad2.js
+++ zad2.js
@@ -1,69 +1,89 @@
 var file = require('file');
 var fs = require('fs');
 
+var sumAsync = 0;
+var fileNumber = [];
+
+var filesNumber = 0;
+var filesArray = [];
+var sumSync = 0;
+var dirNumber;
+
+function synchronizedCountSingleFile(label) {
+
+    var singleFileSum = 0;
+    if (filesNumber > 0) {
+        filesNumber--;
+        fs.createReadStream(filesArray[filesNumber]).on('data', function (chunk) {
+            singleFileSum += chunk.toString()
+                .split(/\r\n|[\n\r\u0085\u2028\u2029]/g)
+                .length - 1;
+        }).on('end', function () {
+            sumSync += singleFileSum;
+            if (filesNumber === 0) {
+                console.timeEnd(label);
+            }
+            synchronizedCountSingleFile(label);
+        }).on('error', function (err) {
+            console.error(err);
+        });
+    }
+    else{
+        console.log(sumSync)
+    }
+}
 
 function sync(directory, label) {
-    var sum = 0;
-    var dirNumber = fs.readdirSync(directory).length;
 
     console.time(label);
 
     file.walkSync(directory, function (dirPath, dirs, files) {
-        var filesNumber = files.length;
-
         files.forEach(function (file) {
-
-            //singleFileSum lines for each file
-            var singleFileSum = 0;
-
-            fs.createReadStream(dirPath + "\\" + file).on('data', function (chunk) {
-                singleFileSum += chunk.toString()
-                    .split(/\r\n|[\n\r\u0085\u2028\u2029]/g)
-                    .length - 1;
-            }).on('end', function () {
-                sum += singleFileSum;
-                filesNumber--;
-                if (filesNumber === 0) dirNumber--;
-                if (dirNumber === 0) {
-                    console.timeEnd(label)
-                }
-            }).on('error', function (err) {
-                console.error(err);
-            });
+            filesArray[filesNumber] = dirPath + "\\" + file;
+            filesNumber++;
         })
     })
+    synchronizedCountSingleFile(label);
 }
 
 
+function countSingleFileLines(file, dirPath, label) {
+    var singleFileSum = 0;
+    fs.createReadStream(file).on('data', function (chunk) {
+        singleFileSum += chunk.toString()
+            .split(/\r\n|[\n\r\u0085\u2028\u2029]/g)
+            .length - 1;
+    }).on('end', function () {
+        sumAsync += singleFileSum;
+        fileNumber[dirPath]--;
+        if (fileNumber[dirPath] === 0) dirNumber--;
+        if (dirNumber === 0) {
+            console.timeEnd(label);
+            console.log(sumAsync)
+        }
+    }).on('error', function (err) {
+        console.error(err);
+    });
+}
+
 function async(directory, label) {
-    var sum = 0;
-    var dirNumber = fs.readdirSync(directory).length;
+    dirNumber = fs.readdirSync(directory).length;
 
     console.time(label);
     file.walk(directory, function (nul, dirPath, dirs, files) {
-        var fileNumber = files.length;
-
+        fileNumber[dirPath] = files.length;
         files.forEach(function (file) {
-            var singleFileSum = 0;
-            fs.createReadStream(file).on('data', function (chunk) {
-                singleFileSum += chunk.toString()
-                    .split(/\r\n|[\n\r\u0085\u2028\u2029]/g)
-                    .length - 1;
-            }).on('end', function () {
-                sum += singleFileSum;
-                // console.log(file, singleFileSum);
-                fileNumber--;
-                if (fileNumber === 0) dirNumber--;
-                if (dirNumber === 0) {
-                    console.timeEnd(label)
+
+            process.nextTick(
+                function () {
+                    countSingleFileLines(file, dirPath, label);
                 }
-            }).on('error', function (err) {
-                console.error(err);
-            });
+            );
         })
 
     });
 
 }
 
-async('./PAM08','async1');
+
+sync("./PAM08", "async");
